be2077328f9d8a80b556d8e9188852ba723f382e
joesindhu <joe.sindhu06@gmail.com>
2017-06-05 18:30:47 +0530

    number.java

---
+++ number.java
@@ -0,0 +1,17 @@
+# joe
+import java.io.*;
+import java.util.*;
+class number
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(system.in);
+int n=s.nextInt();
+if(n<0)
+System.out.println("negative");
+else if(n==0)
+System.out.println("zero");
+else
+System.out.println("positive");
+}
+}
