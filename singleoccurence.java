526d2d7ae1de7ee41d0a00b9ebc4c640b71b21cf
joesindhu <joe.sindhu06@gmail.com>
2017-06-06 17:12:55 +0530

    singleoccurence.java

---
+++ singleoccurence.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class singleoccurence
+{
+public static void main(String args[])
+{
+int i,j;
+int arr[]=new int[args.length];
+for(i=0;i<args.length;i++)
+arr[i]=Integer.parseInt(args[i]);
+int occ[]=new int[arr.length];
+for(i=0;i<arr.length;i++)
+{
+for(j=0;j<arr.length;j++)
+{
+if(arr[i]==arr[j])
+{
+occ[i]+=1;
+}}}
+for(i=0;i<arr.length;i++)
+{
+if(occ[i]==1)
+System.out.println(arr[i]);
+}
+}}
